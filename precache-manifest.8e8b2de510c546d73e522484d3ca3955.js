02366178a8ca5919fc8fa253b0a17271f2aabb9a
Injac92 <injacigor92@gmail.com>
2019-05-01 14:00:39 +0200

    Updates

---
+++ precache-manifest.8e8b2de510c546d73e522484d3ca3955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1377b8f531cbbfb0d18c6dc0d9e828e2",
+    "url": "/tv_shows/static/media/ops1.1377b8f5.jpg"
+  },
+  {
+    "revision": "f74c8bd909f3721a75d4a182f79b8746",
+    "url": "/tv_shows/static/media/2b.f74c8bd9.jpg"
+  },
+  {
+    "revision": "fbfca74a57f9e93911d0",
+    "url": "/tv_shows/static/js/runtime~main.4a17e285.js"
+  },
+  {
+    "revision": "72fb7fd50568b85fc05a",
+    "url": "/tv_shows/static/js/main.0f19aafa.chunk.js"
+  },
+  {
+    "revision": "e4e4ba534fa868524fa7",
+    "url": "/tv_shows/static/js/2.4cb12041.chunk.js"
+  },
+  {
+    "revision": "72fb7fd50568b85fc05a",
+    "url": "/tv_shows/static/css/main.5c115696.chunk.css"
+  },
+  {
+    "revision": "613cb58d5308f4b03501a4b653dfd123",
+    "url": "/tv_shows/index.html"
+  }
+];
