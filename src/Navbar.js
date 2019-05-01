1f4a199fac0d9ae3cdc03ff02f005c3b77af96d5
Injac92 <injacigor92@gmail.com>
2019-05-01 14:17:33 +0200

    changed a to Link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,13 @@
 import React from "react"
-//import {NavLink} from "react-router-dom"
+import {Link} from "react-router-dom"
 
 export default function Navbar () {
     return(
         <div>
-            {/* <NavLink to="/">Home</NavLink>
-            <NavLink to="/series">Series</NavLink>
-            <NavLink to="/favorites">Favorites</NavLink> */}
-            
             <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-                <a className="navbar-brand" href="/">Home </a>
-                <a className="navbar-brand" href="/series">Series </a>
-                <a className="navbar-brand" href="/favorites">Favorites </a>
+                <Link className="navbar-brand" href="/">Home </Link>
+                <Link className="navbar-brand" href="/series">Series </Link>
+                <Link className="navbar-brand" href="/favorites">Favorites </Link>
             </nav>
         </div>
     )
